0c10187b17c948818366dc67f3659e4b6bf3ab5a
Frederick J <36030557+FJvanEeden@users.noreply.github.com>
2021-07-08 11:36:32 +0200

    Added thumbnails component

--- src/Projects.js
+++ src/Projects.js
@@ -1,10 +1,18 @@
 
 import React from "react"
+import Thumbnail from './Thumbnail.js';
+import './App.css';
  
 function Projects(props) {
   return (
     <div>
       <h1>Projects</h1>
+      <Thumbnail
+        link="" // route
+        image=""
+        title=""
+        category=""
+      />
     </div>
   )
 }
