44115159a0fd46bc849105092c27eaa96049c8a1
Frederick J <36030557+FJvanEeden@users.noreply.github.com>
2021-07-08 13:51:28 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import { BrowserRouter, Route, Link } from 'react-router-dom'; 
-import Projects from './Projects.js'; 
-import Articles from './Articles.js'; 
-import About from './About.js'; 
-import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
 
-    <BrowserRouter>
-      <div className="App">
-  
-        <Route exact path="/" baseName={process.env.PUBLIC_URL}/>
-        <Route exact path="/projects" component={Projects} />
-        <Route path="/articles" component={Articles} />
-        <Route path="/about" component={About} />
-
-        <div className="navigation">
-          {/* <img src={logo} className="logo-image" alt="Logo Image" /> */}
-          <div className="navigation-sub">
-            <Link to="/" className="item">Home</Link>                      
-            <Link to="/projects" className="item">Projects</Link>
-            <Link to="/articles" className="item">Articles</Link>
-            <Link to="/about" className="item">About</Link>
-          </div>
-        </div>
-
-        <img src={logo} className="App-logo" alt="logo" />
-
-      </div>
-    </BrowserRouter>
+    <main className="text-gray-400 bg-gray-900 body font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
 
   );
 }
